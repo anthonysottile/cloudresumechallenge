4a357358453f6b7a04cc7626bde21ff44b03371d
anthonysottile <tsottile@hotmail.com>
2025-01-03 17:27:45 -0500

    modify API gateway ordering further and now works on first go

--- main.tf
+++ main.tf
@@ -300,9 +300,7 @@ resource "aws_lambda_permission" "api_gateway_lambda" {
 resource "aws_api_gateway_deployment" "visitor_counter_deployment" {
   rest_api_id = aws_api_gateway_rest_api.visitor_counter_api.id
 
-  triggers = {
-    redeployment = "${timestamp()}"
-  }
+  depends_on = [ aws_api_gateway_method_response.api_method_response ]
 }
 
 resource "aws_api_gateway_stage" "visitor_counter_stage" {
